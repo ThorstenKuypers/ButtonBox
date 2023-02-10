be7cf01f4ea01fad457a5a24820a26a9827fe493
v8judd <v8judd@gmail.com>
2023-02-10 19:55:02 +0100

    - no running with timer to generate simulated button presses - fixed error preventing further functioning, because TXINI interrupt for EP1 is always set and thus generate an interrupt

--- usb-fw/src/main.cpp
+++ usb-fw/src/main.cpp
@@ -1,4 +1,6 @@
 
+// #define F_CPU 16000000UL
+
 #include <avr/io.h>
 #include <avr/interrupt.h>
 #include <util/delay.h>
@@ -6,11 +8,12 @@
 
 #include "ReportDesc.h"
 
-#ifndef F_CPU
-#error "F_CPU not set!"
-#endif
+// #ifndef F_CPU
+// #error "F_CPU not set!"
+// #endif
 
-struct USB_Setup {
+struct USB_Setup
+{
     uint8_t bmRequestType;
     uint8_t bRequest;
     uint8_t wValueL;
@@ -36,19 +39,17 @@ const uint8_t devDesc[18] = {
     0,
     0,
     0,
-    1
-};
+    1};
 
 const uint8_t configDescriptor[9] = {
     9, // bLength
     2, // bDescriptorType
-    34,00,
+    34, 00,
     1,
     1,
     0,
     0x80,
-    50
-};
+    200};
 
 const uint8_t interfaceDescriptor[9] = {
     9,
@@ -59,16 +60,15 @@ const uint8_t interfaceDescriptor[9] = {
     3,
     0,
     0,
-    0
-};
+    0};
 
 const uint8_t hidDescriptor[9] = {
-    9, // bLength
+    9,    // bLength
     0x21, // HID descriptor
     0x10, 0x01,
     0,
     1,
-    0x22, // report descriptor
+    0x22,                          // report descriptor
     sizeof(hidReportDescriptor), 0 // report descriptor length
 };
 
@@ -78,36 +78,93 @@ const uint8_t endpointDescriptor[7] = {
     0b10000001,
     0b00000011,
     8, 0,
-    8
-};
+    8};
 
 volatile uint8_t cnt = 0;
 volatile uint8_t buttons = 0;
 volatile uint8_t btnUpdate = 0;
 volatile uint8_t usbState = 0;
+volatile uint16_t millis = 0;
 
 #define RX_LED PD4
 #define TX_LED PD5
+#define SET_ADDRESS 5
+#define GET_DESCRIPTOR 6
 
-void WaitTx()
+inline void WaitTx()
 {
-    while(!(UEINTX & _BV(TXINI))) {}
+    while (!(UEINTX & _BV(TXINI)))
+    {
+    }
 }
 
 void initEp0()
 {
-    //cli();
     UENUM = 0; // EP0
     UECONX = 0;
     UECFG0X = 0;
-    UECFG1X = (1<<EPSIZE0)|(1<<EPSIZE1)|(1<<ALLOC); // alloc 64 bytes for EP0
-    UECONX |= _BV(EPEN); // enable EP0
-    UEIENX = (1<<RXSTPE);
-    UDIEN = (1<<SOFE)|(1<<EORSTE);
-    //sei();
+    UECFG1X = (1 << EPSIZE0) | (1 << EPSIZE1) | (1 << ALLOC); // alloc 64 bytes for EP0
+    UECONX |= _BV(EPEN);                                      // enable EP0
+    UEIENX = (1 << RXSTPE);
+    UDIEN = (1 << SOFE) | (1 << EORSTE);
 
     if (!(UESTA0X & _BV(CFGOK)))
         PORTD &= ~_BV(RX_LED);
+}
+
+int main(void)
+{
+    DDRD = _BV(RX_LED) | _BV(TX_LED);  // RX/TX led pins as output
+    PORTD = _BV(RX_LED) | _BV(TX_LED); // ports high = LEDs off (LEDs are pulled up to 5V)
+
+    cli();
+    USBCON = _BV(USBE) | _BV(FRZCLK);
+    // config PLL interface
+    PLLCSR = (1 << PLLE) | (1 << PLLP0);
+    while (!(PLLCSR & (1 << PLOCK))) // wait until PLL locked
+    {
+    }
+    // enable USB controller and unfreeze clock
+    USBCON = 0x80; // (1<<USBE)
+    initEp0();
+    UDCON = 0; //&= ~(1<<DETACH); // attach device
+    // timer1
+    // TCCR0A = _BV(WGM01);
+    TCCR1B = _BV(CS10) | _BV(CS11) | _BV(WGM12);
+    OCR1A = 250 - 1;
+    TIMSK1 = _BV(OCIE1A);
+
+    sei();
+
+    while (true)
+    {
+        if (usbState == 2)
+        {
+            // send back HID report
+            if (btnUpdate != 0)
+            {
+                UENUM = 1;
+                UEDATX = 0x01; // report ID
+                UEDATX = buttons;
+                UEINTX &= ~(_BV(TXINI) | _BV(FIFOCON));
+                WaitTx();
+                btnUpdate = 0;
+            }
+        }
+    }
+}
+
+ISR(TIMER1_COMPA_vect)
+{
+    if (1000 == millis)
+    {
+        millis = 0;
+        btnUpdate = 1;
+        buttons ^= _BV(0);
+        PORTD ^= _BV(TX_LED);
+    }
+    else
+        millis++;
 }
 
 // Generic USB interrupt
@@ -115,21 +172,32 @@ ISR(USB_GEN_vect)
 {
     uint8_t udint = UDINT;
 
-    if (udint & (1<<EORSTI))
+    if (udint & (1 << EORSTI))
     {
         // end-of-reset
         initEp0();
-        UDINT &= ~(1<<EORSTI);
+        UDINT &= ~(1 << EORSTI);
+        TCNT1 = 0;
+        // TIMSK1 |= _BV(OCIE1A);
+        // MCUSR &= ~_BV(USBRF);
+
+        return;
     }
 
     if (udint & _BV(SOFI)) // start of frame sequence found; happens every 1ms
     {
-        UDINT &= ~(1<<SOFI);
+        UDINT &= ~(1 << SOFI);
+        // if (1000 == millis)
+        // {
+        //     millis = 0;
+        //     btnUpdate = 1;
+        //     buttons ^= _BV(0);
+        //     PORTD ^= _BV(RX_LED);
+        // }
+        // millis++;
+        return;
     }
 }
-
-#define SET_ADDRESS 5
-#define GET_DESCRIPTOR 6
 
 // Endpoint interrupt
 ISR(USB_COM_vect)
@@ -158,8 +226,9 @@ ISR(USB_COM_vect)
                 {
                     UEDATX = devDesc[i];
                 }
-                UEINTX = ~_BV(TXINI);
-                WaitTx();
+                UEINTX &= ~_BV(TXINI);
+                //WaitTx();
+                return;
             }
             if (2 == stp[3])
             {
@@ -183,8 +252,9 @@ ISR(USB_COM_vect)
                         UEDATX = endpointDescriptor[i];
                     }
                 }
-                UEINTX = ~_BV(TXINI);
-                WaitTx();
+                UEINTX &= ~_BV(TXINI);
+                //WaitTx();
+                return;
             }
             if (4 == stp[3])
             {
@@ -192,7 +262,8 @@ ISR(USB_COM_vect)
                 {
                     UEDATX = interfaceDescriptor[i];
                 }
-                UEINTX = ~_BV(TXINI);
+                UEINTX &= ~_BV(TXINI);
+                return;
             }
             if (0 == stp[2] && 0x22 == stp[3])
             {
@@ -201,11 +272,9 @@ ISR(USB_COM_vect)
                 {
                     UEDATX = hidReportDescriptor[i];
                 }
-                UEINTX = ~_BV(TXINI);
-                WaitTx();
-
-                PORTD &= ~_BV(RX_LED);
+                UEINTX &= ~_BV(TXINI);
                 usbState = 2;
+                return;
             }
         }
         else if (5 == stp[1]) // set address
@@ -214,85 +283,36 @@ ISR(USB_COM_vect)
             UEINTX &= ~_BV(TXINI); // set empty package back to acknowledge address
             WaitTx();
             UDADDR |= _BV(ADDEN);
+
+            return;
         }
         else if (9 == stp[1])
         {
             // set config
-            UEINTX = ~_BV(TXINI);
+            UEINTX &= ~_BV(TXINI);
             WaitTx();
 
             // the device is now in the configured state; so setup additional endpoint(s)
             UENUM = 1; // select EP1
-            UECFG0X = _BV(EPTYPE0) | _BV(EPTYPE1)|_BV(EPDIR);
+            UECFG0X = _BV(EPTYPE0) | _BV(EPTYPE1) | _BV(EPDIR);
             UECFG1X = _BV(ALLOC);
             UECONX = _BV(EPEN);
-            UEIENX = _BV(TXINE); // enable IN interrupt for EP1
 
             UENUM = 0;
             usbState = 1;
+
+            return;
         }
         else if (0x0a == stp[1])
         {
-            // SET_IDLE 
+            // SET_IDLE
             // the device is configured and ready, but the host has not got
             // the report descriptor. So it requests to hold any HID reports
             // for now. Because it can't interpret them yet.
-            UEINTX = ~_BV(TXINI);
+            UEINTX &= ~_BV(TXINI);
             WaitTx();
+
+            return;
         }
-
-        return;
-    }
-    
-}
-
-int main(void)
-{
-    DDRD = _BV(RX_LED)|_BV(TX_LED); // RX/TX led pins as output
-    PORTD = _BV(RX_LED)|_BV(TX_LED); // ports high = LEDs off (LEDs are pulled up to 5V)
-
-    cli();
-    USBCON = _BV(USBE)|_BV(FRZCLK);
-    // config PLL interface
-    PLLCSR = (1<<PLLE)|(1<<PLLP0);
-    while(!(PLLCSR & (1<<PLOCK))) // wait until PLL locked
-    {      
-    }
-    // enable USB controller and unfreeze clock
-    USBCON = 0x80;  // (1<<USBE)
-    initEp0();
-    UDCON = 0; //&= ~(1<<DETACH); // attach device
-    sei();
-
-    while (true)
-    {
-        if (usbState == 2)
-        {
-            // send back HID report
-            if (btnUpdate)
-            {
-                PORTD ^= _BV(TX_LED);
-                WaitTx();
-                UENUM = 1;
-                UEDATX = 0x01; // report ID
-                UEDATX = buttons;
-                UEINTX &= ~(_BV(TXINI) | _BV(FIFOCON));
-                WaitTx();
-                // button state changed?
-                // clear tx and fifocon bits
-                btnUpdate = 0;
-            }
-        }
-
-        if (250 == cnt)
-        {
-            cnt = 0;
-            btnUpdate = 1;
-            buttons ^= _BV(0);
-        }
-
-        cnt++;
-
-        _delay_ms(2);
     }
 }
